22625aef0c2a1f096134a7affaef112711a1185a
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-11-10 15:16:00 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.2b3737i.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 // middlewere
 app.use(cors())
@@ -11,6 +22,23 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('welcome')
 })
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect()
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
     console.log(`my project listening on port : ${port}`)
